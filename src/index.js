b87ce6cf74229ec9cc5bf62b73da9950115cc227
Nicole Samanich <nicolesamanich@gmail.com>
2020-04-30 09:53:53 -0400

    add styles

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import Game from './game.js'; 
 import './styles/index.scss';
-let game = new Game(); 
+// import * as firebase from 'firebase/app';
+// import 'firebase/database';
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyB7upg9r9T9hT3ChSuRfWyjsXIYK7Iv-9k",
+//     authDomain: "scuubaduu.firebaseapp.com",
+//     databaseURL: "https://scuubaduu.firebaseio.com",
+//     projectId: "scuubaduu",
+//     storageBucket: "scuubaduu.appspot.com",
+//     messagingSenderId: "209414285335",
+//     appId: "1:209414285335:web:cfe5f81e7cc62d3d895452",
+//     measurementId: "G-7FMJZ5BQNY"
+// };
+
+let game = new Game();
