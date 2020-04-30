367005e980be8881174b0dc45217c7a54dd5b6dc
Nicole Samanich <nicolesamanich@gmail.com>
2020-04-30 12:00:04 -0400

    write score data to firebase

--- src/index.js
+++ src/index.js
@@ -1,17 +1,35 @@
 import Game from './game.js'; 
 import './styles/index.scss';
-// import * as firebase from 'firebase/app';
-// import 'firebase/database';
+import * as firebase from "firebase/app";
+import 'firebase/database';
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyB7upg9r9T9hT3ChSuRfWyjsXIYK7Iv-9k",
-//     authDomain: "scuubaduu.firebaseapp.com",
-//     databaseURL: "https://scuubaduu.firebaseio.com",
-//     projectId: "scuubaduu",
-//     storageBucket: "scuubaduu.appspot.com",
-//     messagingSenderId: "209414285335",
-//     appId: "1:209414285335:web:cfe5f81e7cc62d3d895452",
-//     measurementId: "G-7FMJZ5BQNY"
-// };
+
+const config = {
+    apiKey: "AIzaSyBsOt6NM1cZ8f4sk95odFm3TuAR6--pzLk",
+    authDomain: "scuuba-duu.firebaseapp.com",
+    databaseURL: "https://scuuba-duu.firebaseio.com",
+    projectId: "scuuba-duu",
+    storageBucket: "scuuba-duu.appspot.com",
+    messagingSenderId: "465224114343",
+    appId: "1:465224114343:web:f97b0e7b19fca1d4f13b82"
+};
+
+  firebase.initializeApp(config);
+  var database = firebase.database();
+
+
+
+
+// write user score data to the db
+
+  function writeUserData(name, value) {
+      debugger; 
+      firebase.database().ref('scores/' + name).set({
+          name: name,
+          value: value,
+      });
+  }
+
+//   writeUserData("nikki", 2000);
 
 let game = new Game();
